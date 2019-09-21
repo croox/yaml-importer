c47a702c5860d9b07730e99beb7d7a1ea4184461
jhotadhari <tellme@waterproof-webdesign.info>
2019-09-21 13:11:11 +0545

    Build successful

---
+++ yaml-importer.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	Plugin Name: Yaml Importer
+	Plugin URI: https://github.com/croox/yaml-importer
+	Description: Import Posts from YAML
+	Version: 0.0.0
+	Author: croox
+	Author URI: https://github.com/croox
+	License: GNU General Public License v2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+	Text Domain: yaim
+	Domain Path: /languages
+	Tags: import yaml bulk
+	GitHub Plugin URI: https://github.com/croox/yaml-importer
+	Release Asset: true
+*/
+?><?php
+
+/**
+ * This is a dummy plugin file, required by GitHub Updater Plugin (https://github.com/afragen/github-updater).
+ * GitHub Updater Plugin will query this file for updates.
+ */
+
+?>
